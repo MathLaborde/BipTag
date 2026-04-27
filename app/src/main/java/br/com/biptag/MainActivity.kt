49d440fdd92aae43986514320a604ed54691a875
Matheus Laborde <mlabordebr@gmail.com>
2026-04-26 21:10:29 -0300

    Inicializando projeto BipTag

---
+++ app/src/main/java/br/com/biptag/MainActivity.kt
@@ -0,0 +1,19 @@
+package br.com.biptag
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import br.com.biptag.ui.theme.BipTagTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BipTagTheme {
+
+            }
+        }
+    }
+}
---
+++ app/src/main/java/br/com/biptag/screens/LoginScreen.kt
@@ -0,0 +1,18 @@
+package br.com.biptag.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import br.com.biptag.ui.theme.BipTagTheme
+
+@Composable
+fun LoginScreen() {
+
+}
+
+@Preview
+@Composable
+private fun LoginScreenPreview() {
+    BipTagTheme() {
+        LoginScreen()
+    }
+}
